822ef4beb28bb8dbe637caac5586ba777cca642f
Yashdiq Lubis <yashdiq@gmail.com>
2025-01-08 17:04:18 +0700

    🐞 fix(React 19): compatibility added

--- src/index.tsx
+++ src/index.tsx
@@ -2,30 +2,31 @@ import React, { useEffect, useRef, useState } from "react";
 import * as lottie from "lottie-web";
 
 export interface LottieProps {
-  path: string
-  loop?: boolean
-  autoPlay?: boolean
-  width?: number | string
-  height?: number | string
-  style?: React.CSSProperties
-  speed?: number
-  direction?: lottie.AnimationDirection
+  path: string;
+  loop?: boolean;
+  autoPlay?: boolean;
+  width?: number | string;
+  height?: number | string;
+  style?: React.CSSProperties;
+  speed?: number;
+  direction?: lottie.AnimationDirection;
 }
 
 const Lottie: React.FC<LottieProps> = (props) => {
-  const {path, width, height, autoPlay, loop, speed, direction, style} = props
+  const { path, width, height, autoPlay, loop, speed, direction, style } =
+    props;
 
-  let divRef = useRef<HTMLDivElement>()
+  let divRef = useRef<HTMLDivElement | null>(null);
 
-  const [animation, setAnimation] = useState<lottie.AnimationItem>()
+  const [animation, setAnimation] = useState<lottie.AnimationItem>();
 
   const getSize = (initial?: number | string) => {
     let size;
-    
-    if (initial && typeof initial === 'number') {
+
+    if (initial && typeof initial === "number") {
       size = `${initial}px`;
     } else {
-      size = initial || '100%';
+      size = initial || "100%";
     }
 
     return size;
@@ -34,11 +35,11 @@ const Lottie: React.FC<LottieProps> = (props) => {
   const styles = {
     width: getSize(width),
     height: getSize(height),
-    overflow: 'hidden',
-    margin: '0 auto',
-    outline: 'none',
+    overflow: "hidden",
+    margin: "0 auto",
+    outline: "none",
     ...style,
-  }
+  };
 
   useEffect(() => {
     let anim: lottie.AnimationItem;
@@ -50,32 +51,26 @@ const Lottie: React.FC<LottieProps> = (props) => {
         autoplay: autoPlay ?? true,
         path: path,
         rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
+          preserveAspectRatio: "xMidYMid slice",
         },
-      })
+      });
 
-      setAnimation(anim)
-      
-      anim.setDirection(direction ?? 1)
-      anim.setSpeed(speed ?? 1)
+      setAnimation(anim);
+
+      anim.setDirection(direction ?? 1);
+      anim.setSpeed(speed ?? 1);
     }
 
     return () => {
-      animation?.destroy()
+      animation?.destroy();
     };
-  }, [])
+  }, []);
 
   const onClick = () => {
-    animation?.isPaused ?  animation?.play() : animation?.pause()
-  }
+    animation?.isPaused ? animation?.play() : animation?.pause();
+  };
 
-  return (
-    <div
-      ref={ref => divRef.current = ref ?? undefined}
-      style={styles}
-      onClick={onClick}
-    ></div>
-  )
-}
+  return <div ref={divRef} style={styles} onClick={onClick}></div>;
+};
 
-export default Lottie
+export default Lottie;
